f135a5c9bd953414678ca09e5cff19d25a2fb9d0
Daniel Macbubi <daniel.macbubi@zoominfo.com>
2019-01-01 19:13:20 +0200

    Adds simple chart with dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlantService } from './services/plant-data.service';
+import { Chart } from 'chart.js';
+import {ElementRef, ViewChild} from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,65 @@ import { PlantService } from './services/plant-data.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  chart;
   title = 'plantio';
-  measures = [];
-  constructor(private plantService: PlantService) { }
+  measures = [
+    1,2,3,4,3,5,7
+  ];
+  dataLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  config = {
+    type: 'line',
+    data: {
+      labels: this.dataLabels,
+      datasets: [{
+        label: 'Water (mm)',
+        fill: false,
+        backgroundColor: 'blue',
+        borderColor: 'blue',
+        data: this.measures,
+      }]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Month'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Value'
+          }
+        }]
+      }
+    }
+  };
+  @ViewChild('chartCanvas') chartCanvas: ElementRef;
   
+  constructor(private plantService: PlantService) { }
+
   ngOnInit() {
-    this.plantService.getPlantData('','','').subscribe((measures: any[]) => this.measures = measures);
+    this.plantService.getPlantData('', '', '').subscribe((measures: any[]) => {
+      this.measures = measures
+      this.chart = new Chart(this.chartCanvas.nativeElement.getContext('2d'), this.config);
+      debugger
+    });
   }
 }
