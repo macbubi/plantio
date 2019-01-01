c75997599d6ea79dcca851890611ca681bb6046a
Daniel Macbubi <daniel.macbubi@zoominfo.com>
2019-01-01 19:41:54 +0200

    finish char for all incoming data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlantService } from './services/plant-data.service';
 import { Chart } from 'chart.js';
-import {ElementRef, ViewChild} from "@angular/core";
+import { ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -9,65 +9,66 @@ import {ElementRef, ViewChild} from "@angular/core";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  private MEASURE_TYPES = [{
+    name: "humidity",
+    label: "Humidity",
+    color: '#ccc'
+  }, {
+    name: "moisture",
+    label: "Moisture",
+    color: 'gray'
+  }, {
+    name: "sunlight",
+    label: "Sunlight",
+    color: 'yellow'
+  }, {
+    name: "temprature",
+    label: "Temprature",
+    color: '#c6b152'
+  }];
   chart;
   title = 'plantio';
-  measures = [
-    1,2,3,4,3,5,7
-  ];
-  dataLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  config = {
-    type: 'line',
-    data: {
-      labels: this.dataLabels,
-      datasets: [{
-        label: 'Water (mm)',
-        fill: false,
-        backgroundColor: 'blue',
-        borderColor: 'blue',
-        data: this.measures,
-      }]
-    },
-    options: {
-      responsive: true,
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart'
-      },
-      tooltips: {
-        mode: 'index',
-        intersect: false,
-      },
-      hover: {
-        mode: 'nearest',
-        intersect: true
-      },
-      scales: {
-        xAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Month'
-          }
-        }],
-        yAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Value'
-          }
-        }]
-      }
-    }
-  };
+  measures = [];
+
+
   @ViewChild('chartCanvas') chartCanvas: ElementRef;
-  
+
   constructor(private plantService: PlantService) { }
 
   ngOnInit() {
     this.plantService.getPlantData('', '', '').subscribe((measures: any[]) => {
-      this.measures = measures
-      this.chart = new Chart(this.chartCanvas.nativeElement.getContext('2d'), this.config);
-      debugger
+      this.measures = measures;
+      this.chart = new Chart(this.chartCanvas.nativeElement.getContext('2d'), this.getChartConfig());
     });
+  }
+
+  getChartConfig() {
+    return {
+      type: 'line',
+      data: {
+        labels: this.measures.map((measure) => measure.measuredAt),
+        datasets: this.MEASURE_TYPES.map((measureType) => {
+          return {
+            label: measureType.label,
+            fill: false,
+            backgroundColor: measureType.color,
+            borderColor: measureType.color,
+            data: this.measures.map((measure) => measure[measureType.name]),
+          };
+        })
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true
+        },
+        
+      }
+    };
   }
 }
